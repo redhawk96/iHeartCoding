96df75e092d1a020ba230c9b6e9eac3669e376f0
redhawk96 <it17038492@my.sliit.lk>
2019-05-10 21:41:29 +0530

    Validating form
    Redirecting to previous page if form fields are empty/ blank

--- controllers/articleController.php
+++ controllers/articleController.php
@@ -13,34 +13,33 @@ $article = new Article();
 // To add new article
 if(isset($_POST['add_new_article'])){
 
-    
-    $a_title = $_POST['a_title'];
-    $a_tags = $_POST['a_tags'];
-    $a_cat_id = $_POST['a_cat_id'];
-    $a_author = $_POST['a_author'];
-    $a_status = $_POST['a_status'];
-
-    // Getting image name 
-    $a_image = $_FILES["a_image"]["name"];
-
-    // Getting temporay image name
-    $a_image_temp = $_FILES['a_image']['tmp_name'];
-
-    // Getting image extention
-    $a_image_extension = end(explode(".", $a_image));
-
-    // Renaming the image with unique number and changing the imge extention to png
-    $a_image_final = round(microtime(true)) ."."."png";
-
-    $a_content = $_POST['a_content'];
-    $a_com_count = 1;
-
-
     if($a_title == "" || empty($a_title) || $a_tags == "" || empty($a_tags) || $a_cat_id == "" || empty($a_cat_id) || $a_author == "" || empty($a_author) || $a_status == "" || empty($a_status) || $a_content == "" || empty($a_content)){
     
-        header('Location: ../Admin/publish-article.php?a=false');
+        header('Location: ../Admin/publish-article?a=false');
     
     }else{
+
+        $a_title = $_POST['a_title'];
+        $a_tags = $_POST['a_tags'];
+        $a_cat_id = $_POST['a_cat_id'];
+        $a_author = $_POST['a_author'];
+        $a_status = $_POST['a_status'];
+
+        // Getting image name 
+        $a_image = $_FILES["a_image"]["name"];
+
+        // Getting temporay image name
+        $a_image_temp = $_FILES['a_image']['tmp_name'];
+
+        // Getting image extention
+        $a_image_extension = end(explode(".", $a_image));
+
+        // Renaming the image with unique number and changing the imge extention to png
+        $a_image_final = round(microtime(true)) ."."."png";
+
+        $a_content = $_POST['a_content'];
+        $a_com_count = 1;
+
 
         $addArticle = $article->addArticle($a_cat_id, $a_title, $a_author, $a_image_final, $a_content, $a_tags, $a_com_count, $a_status);
 
@@ -53,7 +52,7 @@ if(isset($_POST['add_new_article'])){
             // If update query was successful then image will be moved into the server
             move_uploaded_file($a_image_temp, "../public/upload/articles/$a_image_final");
 
-            header('Location: ../Admin/articles.php');
+            header('Location: ../Admin/articles');
         }
     }
 }
@@ -61,35 +60,35 @@ if(isset($_POST['add_new_article'])){
 // To update existing article
 if(isset($_POST['update_article'])){
 
-    $a_id = $_POST['update_article'];
-    $a_title = $_POST['a_title'];
-    $a_tags = $_POST['a_tags'];
-    $a_cat_id = $_POST['a_cat_id'];
-    $a_author = $_POST['a_author'];
-    $a_status = $_POST['a_status'];
-    $a_image_name = $_POST['a_image_name'];
-
-    // Getting image name 
-    $a_image = $_FILES["a_image"]["name"];
-
-    // Getting temporay image name
-    $a_image_temp = $_FILES['a_image']['tmp_name'];
-
-    // Getting image extention
-    $a_image_extension = end(explode(".", $a_image));
-
-    // Renaming the image with previous name
-    $a_image_final = $a_image_name;
-
-    $a_content = $_POST['a_content'];
-    $a_com_count = 1;
-
-
     if($a_title == "" || empty($a_title) || $a_tags == "" || empty($a_tags) || $a_cat_id == "" || empty($a_cat_id) || $a_author == "" || empty($a_author) || $a_status == "" || empty($a_status) || $a_content == "" || empty($a_content)){
     
-        header('Location: ../Admin/publish-article.php?a=false');
+        header('Location: ../Admin/publish-article?u=false');
     
     }else{
+
+        $a_id = $_POST['update_article'];
+        $a_title = $_POST['a_title'];
+        $a_tags = $_POST['a_tags'];
+        $a_cat_id = $_POST['a_cat_id'];
+        $a_author = $_POST['a_author'];
+        $a_status = $_POST['a_status'];
+        $a_image_name = $_POST['a_image_name'];
+
+        // Getting image name 
+        $a_image = $_FILES["a_image"]["name"];
+
+        // Getting temporay image name
+        $a_image_temp = $_FILES['a_image']['tmp_name'];
+
+        // Getting image extention
+        $a_image_extension = end(explode(".", $a_image));
+
+        // Renaming the image with previous name
+        $a_image_final = $a_image_name;
+
+        $a_content = $_POST['a_content'];
+        $a_com_count = 1;
+
 
         $updateArticle = $article->updateArticle($a_id, $a_cat_id, $a_title, $a_author, $a_image_final, $a_content, $a_tags, $a_com_count, $a_status);
 
@@ -101,7 +100,7 @@ if(isset($_POST['update_article'])){
             // If update query was successful then image will be moved into the server
             move_uploaded_file($a_image_temp, "../public/upload/articles/$a_image_final");
             
-            header('Location: ../Admin/articles.php');
+            header('Location: ../Admin/articles');
         }
     }
 
@@ -124,7 +123,7 @@ if(isset($_POST['delete_article'])){
         // Deleting image from the server for the specific artice
         unlink("../public/upload/articles/".$a_image_name);
 
-        header('Location: ../Admin/articles.php');
+        header('Location: ../Admin/articles');
     }
 
 }
